6fecb750a9810ffdb3cee809452d2f02127b0f2b
erangad <erangadesaa@gmail.com>
2024-01-31 17:34:37 -0800

    ERP by block into participants

--- src/analysis/processing.py
+++ src/analysis/processing.py
@@ -83,5 +83,83 @@ def eventEpochdata(raw):
     return epochs, epoch_event_dict
 
 
+def eventEpocshByBlocks(raw):
+    # get events and event dict 
+    events_from_annot, event_dict =  mne.events_from_annotations(raw)
+
+    stimcodes =  tools.helpers.getOVStimCodes()
+    stimGroups = tools.helpers.getStimGroups()
+
+    inv_stimCodesMap =  { v: k for k, v in stimcodes.items()}
+    inv_stimGroupsMap = { v: k for k, v in stimGroups.items() }
+
+    # if check to make sure only get the relevant keys 
+    modified_event_dict =  { inv_stimGroupsMap[inv_stimCodesMap[int(k)]] :
+                            v for k,v in event_dict.items() 
+                            if inv_stimCodesMap[int(k)] in inv_stimGroupsMap}
+
+    tmin = configss['epoch_tmin'] if configss['epoch_tmin'] is not None else None 
+    tmax = configss['epoch_tmax'] if configss['epoch_tmax'] is not None else None
+
+    metadata_tmin, metadata_tmax = tmin, tmax
+
+    row_events = [
+    'visual/image_display/onset/stimulus/non_freq',
+    'visual/image_display/onset/stimulus/freq',
+    'visual/image_display/onset/instruction/set_1/instruction_1'
+    ]
+
+
+    metadata, meta_events, meta_event_id = mne.epochs.make_metadata(
+    events=events_from_annot,
+    event_id= modified_event_dict, 
+    row_events = row_events,
+    tmin=metadata_tmin,
+    tmax=metadata_tmax,
+    sfreq=raw.info["sfreq"],
+    keep_first= ['visual/image_display/onset/stimulus']
+    )
+
+    metadata["oddball"] = False
+    metadata["control"] = False
+    metadata.loc[ metadata['first_visual/image_display/onset/stimulus'] == 'non_freq', "oddball" ] = True 
+    metadata.loc[ metadata['first_visual/image_display/onset/stimulus'] == 'freq', "control" ] = True 
+
+    mask = metadata['visual/image_display/onset/instruction/set_1/instruction_1'] == 0.0
+
+    relavent_indexes = metadata[mask].index
+
+    #initialize column
+    metadata.loc[:, "block"] = ""
+
+    for i in range(len(relavent_indexes)):
+        from_index =  relavent_indexes[i -1] if i > 0 else 0
+        to_index = relavent_indexes[i]
+        metadata.loc[from_index: to_index, "block"] = "b" + str(i)
+
+
+    # reject high amplitude signals, could be artifacts
+    reject_criteria = dict(eeg=100e-6)  # 100 µV
+
+
+    # baseline correction
+    baseline_correction_l = configss['baseline_correction_l'] \
+        if configss['baseline_correction_l'] is not None else None 
+    
+    baseline_correction_h = configss['baseline_correction_h'] \
+        if configss['baseline_correction_h'] is not None else None
+
+    epochs = mne.Epochs(raw, meta_events, 
+                        event_id= meta_event_id, tmin=tmin, tmax=tmax,
+                          preload=True, reject=reject_criteria, 
+                          metadata=  metadata,
+                        baseline=(
+                            baseline_correction_l,
+                              baseline_correction_h))
+
+    return epochs, meta_event_id
+
+
+
 def plot():
     pass
